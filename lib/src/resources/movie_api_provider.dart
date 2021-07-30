64d68718b383b0f67c72e6b8f947e10b9ed8e2b8
Salayna <salaynadoukoure@gmail.com>
2021-07-30 10:35:11 +0200

    Functionnal BLoC pattern

---
+++ lib/src/resources/movie_api_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import '../models/item_model.dart';
+
+class MovieApiProvider {
+  Client client = Client();
+  final _apiKey = '1e3b1dace9eae2db56e3bc2bbb0b3fe7';
+
+  Future<ItemModel> fetchMovieList() async {
+    print("entered");
+    var url = Uri.parse("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
+    final response = await client
+        .get(url);
+    print(response.body.toString());
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+}
